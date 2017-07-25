c27496c00faffe5ba06ba95ca053ae54de67eb19
soonhyung <1n01raymond@gmail.com>
2017-07-25 19:22:23 +0900

    ModalType 수정

--- CatGame/Assets/Scripts/Core/UI/UIManager.cs
+++ CatGame/Assets/Scripts/Core/UI/UIManager.cs
@@ -4,36 +4,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+public enum ModalType
+{
+}
+
 namespace Core
 {
-	[Singleton(CreateInstance = true, DontDestroyOnLoad = true, PrefabPath = "UIManager")]
-	public class UIManager : SingletonBehaviour<UIManager>
-	{
-		public EventSystem EventSystem { get; private set; }
-		public Camera Camera { get; private set; }
+    [Singleton(CreateInstance = true, DontDestroyOnLoad = true, PrefabPath = "UIManager")]
+    public class UIManager : SingletonBehaviour<UIManager>
+    {
+        public EventSystem EventSystem { get; private set; }
+        public Camera Camera { get; private set; }
 
-		//모달의 시작 Order
-		private const int kOrderOfModal = 10;
-		//모달간의 Spacing
-		private const int kSpacingOfModal = 10;
+        //모달의 시작 Order
+        private const int kOrderOfModal = 10;
+        //모달간의 Spacing
+        private const int kSpacingOfModal = 10;
 
-		private const string kUIPrefabPath = "UI/Prefab/";
+        private const string kUIPrefabPath = "UI/Prefab/";
 
-		private List<UIModal> modals = new List<UIModal>();
+        private List<UIModal> modals = new List<UIModal>();
         private Dictionary<ModalType, Queue<Action>> modalQueues = new Dictionary<ModalType, Queue<Action>>();
         private Dictionary<ModalType, bool> isQueuedModalOpened = new Dictionary<ModalType, bool>();
 
         protected override void Awake()
-		{
-			base.Awake();
+        {
+            base.Awake();
 
-			EventSystem = GetComponentInChildren<EventSystem>();
-			Camera = GetComponentInChildren<Camera>();
-		}
+            EventSystem = GetComponentInChildren<EventSystem>();
+            Camera = GetComponentInChildren<Camera>();
+        }
 
-		public void Init()
-		{
-		}
+        public void Init()
+        {
+        }
 
         public void EnqueueModal(ModalType type, Action modalCreateAction)
         {
@@ -73,54 +77,54 @@ namespace Core
         }
 
         public void PushModal(UIModal modal)
-		{
-			modals.Add(modal);
+        {
+            modals.Add(modal);
 
-			UpdateModalOrders();
-		}
+            UpdateModalOrders();
+        }
 
-		public void PopModal(UIModal modal)
-		{
-			modals.Remove(modal);
+        public void PopModal(UIModal modal)
+        {
+            modals.Remove(modal);
 
-			UpdateModalOrders();
-		}
+            UpdateModalOrders();
+        }
 
-		private void UpdateModalOrders()
-		{
-			for (int i = 0; i < modals.Count; i++)
-			{
-				var modal = modals[i];
-				modal.Canvas.sortingOrder = kOrderOfModal + (kSpacingOfModal * i);
-			}
-		}
+        private void UpdateModalOrders()
+        {
+            for (int i = 0; i < modals.Count; i++)
+            {
+                var modal = modals[i];
+                modal.Canvas.sortingOrder = kOrderOfModal + (kSpacingOfModal * i);
+            }
+        }
 
-		public void BlockEvent()
-		{
-			EventSystem.enabled = false;
-		}
+        public void BlockEvent()
+        {
+            EventSystem.enabled = false;
+        }
 
-		public void UnblockEvent()
-		{
-			EventSystem.enabled = true;
-		}
+        public void UnblockEvent()
+        {
+            EventSystem.enabled = true;
+        }
 
-		//Type, PrepabPath
-		private static Dictionary<Type, string> uiMap = new Dictionary<Type, string>();
+        //Type, PrepabPath
+        private static Dictionary<Type, string> uiMap = new Dictionary<Type, string>();
 
-		public T Open<T>() where T : UIBase
-		{
-			string path;
-			if (uiMap.TryGetValue(typeof(T), out path) == false)
-			{
-				Debug.LogError("UI를 찾을 수 없습니다." + typeof(T).Name);
-				return null;
-			}
+        public T Open<T>() where T : UIBase
+        {
+            string path;
+            if (uiMap.TryGetValue(typeof(T), out path) == false)
+            {
+                Debug.LogError("UI를 찾을 수 없습니다." + typeof(T).Name);
+                return null;
+            }
 
-			var prefab = Resources.Load<T>(path);
-			var ui = Instantiate<T>(prefab);
+            var prefab = Resources.Load<T>(path);
+            var ui = Instantiate<T>(prefab);
 
-			return ui;
-		}
-	}
+            return ui;
+        }
+    }
 }
